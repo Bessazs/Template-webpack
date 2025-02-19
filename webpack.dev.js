b745aa024d5394673326221cb2ca4ce7c59aecdd
Bessa <bessa.vitor06@gmail.com>
2025-02-19 19:41:18 -0300

    Adição arquivo css

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,10 @@
-const { merge } = require('webpack-merge');
-
-const common = require('./webpack.common.js');
-
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-
-  mode: 'development',
-
-  devtool: 'inline-source-map',
-
+  mode: "development",
+  devtool: "eval-source-map",
   devServer: {
-
-    static: './dist',
-
+    watchFiles: ["./src/template.html"],
   },
-
 });
